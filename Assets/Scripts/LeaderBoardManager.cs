6cb18966a8253b03d79ed0b0ed26df21e626eb47
ThanhNH <thanhnh@vitalify.asia>
2020-09-30 00:54:48 +0700

    update to unity 2020 settings

--- Assets/Scripts/LeaderBoardManager.cs
+++ Assets/Scripts/LeaderBoardManager.cs
@@ -2,6 +2,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
 public class LeaderBoardManager : MonoBehaviour {
 	
 	private LeaderBoard lBoard;
